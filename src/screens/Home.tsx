489ad061910f444ff99f0e3dacaf72e4cbaac2d2
AHMET FARUK ACAR <acarahmetfaruk@gmail.com>
2023-09-20 14:15:07 +0300

    database connected

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,21 @@
 import { View, Text, TouchableOpacity, Pressable } from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { HomeScreenNavigationProp } from '../navigation/types'
+import firestore from '@react-native-firebase/firestore';
 
 const Home = () => {
 
     const navigation = useNavigation<HomeScreenNavigationProp>()
+
+    const getData = async () => { 
+      const users = await firestore().collection('campsites').get();
+      console.log(users)
+     }
+
+    useEffect(() => {
+     getData()
+    }, [])
 
   return (
     <View>

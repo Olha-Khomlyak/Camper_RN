e61b28d5950d47c2a4b79fa299dc30ca4c7c1225
AHMET FARUK ACAR <acarahmetfaruk@gmail.com>
2023-09-30 18:55:12 +0300

    react native elemens added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,6 +5,8 @@ import {
   Pressable,
   SafeAreaView,
   FlatList,
+  StyleSheet,
+  Image,
 } from 'react-native';
 import React, {useEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
@@ -14,6 +16,7 @@ import CampStore from '../mobx/CampStore';
 import {getData} from '../functionality/APIs/firebase';
 import {toJS} from 'mobx';
 import {CampInfo} from '../mobx/types';
+import { Icon } from '@rneui/themed';
 
 const Home: React.FC = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
@@ -23,10 +26,40 @@ const Home: React.FC = () => {
   }, []);
 
   const _renderItem = (item: CampInfo) => (
-    <View style={{backgroundColor: 'red', justifyContent: 'space-between', flexDirection:"row"}}>
-      <Text>{item.name}</Text>
+    <View
+      style={{
+        justifyContent: 'space-between',
+        flexDirection: 'row',
+        margin: 4,
+        alignItems: 'center',
+      }}>
+      <View style={{flexDirection: 'row'}}>
+        <Image
+          style={{width: 50, height: 50}}
+          source={{uri: item.imageURLString}}
+        />
+        <View style={{marginLeft: 10}}>
+          <Text>{item.name}</Text>
+          <Text>{item.region}</Text>
+        </View>
+      </View>
+
       <Text>{item.price}</Text>
+      <Icon
+        name='edit'
+        type='material'
+        color='#f50'
+        onPress={() => console.log('hello')} />
     </View>
+  );
+
+  const _separator = () => (
+    <View
+      style={{
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+      }}
+    />
   );
 
   return (
@@ -34,6 +67,7 @@ const Home: React.FC = () => {
       <FlatList
         data={toJS(CampStore.camps)}
         renderItem={({item}) => _renderItem(item)}
+        ItemSeparatorComponent={_separator}
       />
     </SafeAreaView>
   );

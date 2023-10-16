b992e160fd4e2fd358fc984fdb5026635f42e35a
olha-khomlyak <olga.khomlyak@gmail.com>
2023-10-16 17:26:05 +0300

    UI updates

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,6 @@
 import {
   View,
   Text,
-  TouchableOpacity,
-  Pressable,
   SafeAreaView,
   FlatList,
   StyleSheet,
@@ -18,6 +16,7 @@ import {toJS} from 'mobx';
 import {CampInfo} from '../mobx/types';
 import {Icon} from '@rneui/themed';
 import Colors from '../constnats/colors';
+import contStyles from '../constnats/styles';
 
 const Home: React.FC = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
@@ -27,54 +26,49 @@ const Home: React.FC = () => {
   }, []);
 
   const _renderItem = (item: CampInfo) => (
-    <View
-      style={{
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        margin: 5,
-        alignItems: 'center',
-        borderWidth:1,
-        borderColor: Colors.GRAY_ASPARAGUS,
-        padding:5, borderRadius:15
-      }}>
+    <View style={styles.frame}>
       <View style={{flexDirection: 'row'}}>
-        <Image
-          style={{width: 50, height: 50, borderRadius:15}}
-          source={{uri: item.imageURLString}}
-        />
+        <Image style={styles.image} source={{uri: item.imageURLString}} />
         <View style={{marginLeft: 10}}>
-          <Text>{item.name}</Text>
-          <Text>{item.region}</Text>
+          <Text style={contStyles.title}>{item.name}</Text>
+          <Text style={contStyles.text}>{item.region}</Text>
         </View>
       </View>
-
-      <Text>{item.price} TL</Text>
-      <Icon
-        name="chevron-forward-outline"
-        type="ionicon"
-        color={Colors.DARK_VANILLA}
-        onPress={() => console.log('see details')}
-      />
+      <View style={styles.priceBlock}>
+        <Text style={contStyles.text}>{item.price} TL</Text>
+        <Icon
+          name="chevron-forward-outline"
+          type="ionicon"
+          color={Colors.DARK_VANILLA}
+          onPress={() => navigation.navigate("Details", {id: item.id})}
+        />
+      </View>
     </View>
   );
 
-  const _separator = () => (
-    <View
-      style={{
-        borderBottomColor: 'red',
-        borderBottomWidth: StyleSheet.hairlineWidth,
-      }}
-    />
-  );
-
   return (
-    <SafeAreaView style={{flex:1,backgroundColor:"white"}}>
-        <FlatList
-          data={toJS(CampStore.camps)}
-          renderItem={({item}) => _renderItem(item)}
-        />
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <FlatList
+        data={toJS(CampStore.camps)}
+        renderItem={({item}) => _renderItem(item)}
+      />
     </SafeAreaView>
   );
 };
 
 export default observer(Home);
+
+const styles = StyleSheet.create({
+  frame: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    margin: 5,
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: Colors.GRAY_ASPARAGUS,
+    padding: 5,
+    borderRadius: 15,
+  },
+  image: {width: 50, height: 50, borderRadius: 15},
+  priceBlock: {flexDirection: 'row', alignItems: 'center'},
+});

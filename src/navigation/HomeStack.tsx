745549de8eb575551315d9dfb4e265acc0d1e39b
olha-khomlyak <olga.khomlyak@gmail.com>
2023-10-17 16:40:33 +0300

    Details header removed

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -1,17 +1,21 @@
-import * as React from 'react'
-import { MainStack,MainRoutes } from './routes'
+import * as React from 'react';
+import {MainStack, MainRoutes} from './routes';
 
+import Home from '../screens/Home';
+import Details from '../screens/Details';
+import Colors from '../constnats/colors';
 
-import Home from '../screens/Home'
-import Details from '../screens/Details'
+const HomeStackNavigator = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name={MainRoutes.HOME} component={Home} />
+      <MainStack.Screen
+        name={MainRoutes.DETAILS}
+        component={Details}
+        options={({}) => ({headerShown: false})}
+      />
+    </MainStack.Navigator>
+  );
+};
 
-const HomeStackNavigator = () => { 
-    return (
-        <MainStack.Navigator>
-            <MainStack.Screen name={MainRoutes.HOME} component={Home} />
-            <MainStack.Screen name={MainRoutes.DETAILS} component={Details}/>
-        </MainStack.Navigator>
-    )
- }
-
- export default HomeStackNavigator;
+export default HomeStackNavigator;

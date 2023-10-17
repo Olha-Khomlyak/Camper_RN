0a3da97896c946a1b07fc1c7d3151d2aee62f9b6
olha-khomlyak <olga.khomlyak@gmail.com>
2023-10-17 16:40:05 +0300

    + colors

--- src/constnats/colors.ts
+++ src/constnats/colors.ts
@@ -2,7 +2,9 @@ const Colors = {
     RICH_BLACK: "#001524",
     GRAY_ASPARAGUS: "#445D48",
     DARK_VANILLA:"#D6CC99",
-    FLESH:"#FDE5D4"
+    FLESH:"#FDE5D4",
+    WHITE: "#fff",
+    WHITE_TRANSPARENT: "rgba(255, 255, 255, 0.5)"
   };
   
   export default Colors;

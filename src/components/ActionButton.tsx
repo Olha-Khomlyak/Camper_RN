a7c236094dbc4231ccc3a0584aa63bbe05f28e9f
olha-khomlyak <olga.khomlyak@gmail.com>
2023-10-22 22:03:17 +0300

    style fix

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -2,6 +2,7 @@ import {View, Text, Pressable, StyleSheet, Platform, Linking, Alert} from 'react
 import React from 'react';
 import {color} from '@rneui/base';
 import {Icon} from '@rneui/themed';
+import Colors from '../constnats/colors';
 
 interface ActionProps {
   title: string;
@@ -13,7 +14,7 @@ interface ActionProps {
 
 const ActionButton: React.FC<ActionProps> = props => {
   return (
-    <Pressable style={[styles.container, styles.shadow, {backgroundColor: props.btnColor}]} onPress={props.action}>
+    <Pressable style={[styles.container, styles.shadow, {borderColor: props.btnColor, backgroundColor:Colors.WHITE  }]} onPress={props.action}>
       <Icon name={props.iconName} type="ionicon" size={15}  iconStyle={{paddingRight:5}}/>
       <Text>{props.title}</Text>
     </Pressable>
@@ -29,7 +30,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding:5, 
     marginVertical:10,
-    marginRight:10
+    marginRight:10,
+    borderWidth:1
   },
   shadow: {
     ...Platform.select({
